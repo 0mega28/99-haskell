9466fcceb9cc62f63879225a2760497e9ad64c0f
Saksham Gupta <sakshamdam54321@gmail.com>
2024-06-19 00:07:20 +0530

    Q29 and Q30 are missing

--- solutions/29/main.hs
+++
@@ -1,6 +0,0 @@
-isPrime :: Int -> Bool
-isPrime n = notElem 0 $ map (mod n) [2..n-1]
-
-main = do
-    print $ isPrime 7
-    print $ isPrime 39
--- solutions/30/main.hs
+++
@@ -1,8 +0,0 @@
-gcd' 0 b = b
-gcd' a 0 = a
-gcd' a b = abs $ gcd' b (a `mod` b)
-
-main = do
-    print $ gcd' 3 6
-    print $ gcd' (-3) (-6)
-    print $ gcd' (-3) 6
