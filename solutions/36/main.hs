c0ea91fa93346297c44d74cac42b3a086acefc11
Saksham Gupta <sakshamdam54321@gmail.com>
2024-06-19 20:58:56 +0530

    Use existing primeFactors code for primeFactorMulti

--- solutions/36/main.hs
+++ solutions/36/main.hs
@@ -1,3 +1,17 @@
+import Data.List (group)
+
+primeFactors = util 2
+  where
+    util :: Int -> Int -> [Int]
+    util a b
+      | b == 1 = []
+      | b `mod` a == 0 = a : util a (b `div` a)
+      | otherwise = util (a + 1) b
+
+primeFactorMulti' = map encode . group . primeFactors
+  where
+    encode xs = (head xs, length xs)
+
 primeFactorMulti :: Int -> [(Int, Int)]
 primeFactorMulti n = util 2 0 n
   where
@@ -8,7 +22,8 @@ primeFactorMulti n = util 2 0 n
       | acc /= 0 = (i, acc) : util (i + 1) 0 n
       | otherwise = util (i + 1) 0 n
 
-main = do 
-    print $ primeFactorMulti 315
-    print $ primeFactorMulti 48
-    print $ primeFactorMulti 1024
+main = do
+  print $ primeFactorMulti 315
+  print $ primeFactorMulti 48
+  print $ primeFactorMulti 1024
+  print $ primeFactorMulti' 1024
